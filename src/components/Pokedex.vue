13bb04dff4e38844e24731294cfa5b9c2b4c5e06
Yasufumi Nakamoto <nakamo326@gmail.com>
2021-01-18 23:41:27 +0900

    fix typo

---
+++ src/components/Pokedex.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Pokedex.vue</h1>
+    <h2>
+      {{count}}
+    </h2>
+    <div>{{list}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pokedex',
+  data: function () {
+    return {
+      count: '',
+      list: {}
+    }
+  },
+  mounted () {
+    this.$axios
+      .get('https://pokeapi.co/api/v2/pokemon/')
+      .then((res) => {
+        this.count = res.data.count;
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
